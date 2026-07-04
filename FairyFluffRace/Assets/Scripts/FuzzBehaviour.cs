30c787fa7877f93a93f8510bff7e4db518776ffa
Hercules Dias Campos <hercules.amphibolite@gmail.com>
2026-07-04 14:33:26 -0400

    first commit

---
+++ FairyFluffRace/Assets/Scripts/FuzzBehaviour.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class FuzzBehaviour : MonoBehaviour
+{
+    [SerializeField] private float drag;
+    private Rigidbody m_rb;
+
+    void Awake() {
+        m_rb = GetComponent<Rigidbody>();
+        m_rb.linearDamping = drag;
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
